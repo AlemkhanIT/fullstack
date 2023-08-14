bdda5b3cbfeeee32d3ddd13b04a062f689209163
AlemkhanIT <captainkenpachi7@gmail.com>
2023-08-15 01:21:11 +0600

    Middleware, error handler

--- server/index.js
+++ server/index.js
@@ -5,11 +5,15 @@ const models = require('./models/models')
 const PORT = process.env.PORT||5000;
 const cors = require('cors');
 const router = require('./routes/index');
+const errorHandler = require('./middleware/ErrorHandlingMiddleWare')
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api', router);
+
+//last error checker middlware
+app.use(errorHandler)
 
 
 const start = async () => {
